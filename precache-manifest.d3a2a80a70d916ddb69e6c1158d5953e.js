c167525b6f40e170d7bf52b9a3a999b26c45b002
kumarabhishek404 <abhishek19@navgurukul.org>
2020-09-26 22:09:41 +0530

    Updates

---
+++ precache-manifest.d3a2a80a70d916ddb69e6c1158d5953e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ab547013c937086602bc61bf879097e",
+    "url": "/Home%20Photo%20Gallery/index.html"
+  },
+  {
+    "revision": "fb46368a197658ef122d",
+    "url": "/Home%20Photo%20Gallery/static/css/main.e7427e35.chunk.css"
+  },
+  {
+    "revision": "61fb2d24987f32bb1f32",
+    "url": "/Home%20Photo%20Gallery/static/js/2.652f344d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Home%20Photo%20Gallery/static/js/2.652f344d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb46368a197658ef122d",
+    "url": "/Home%20Photo%20Gallery/static/js/main.d8cd926d.chunk.js"
+  },
+  {
+    "revision": "9581735d586c2374a0fa",
+    "url": "/Home%20Photo%20Gallery/static/js/runtime-main.a7c211fb.js"
+  }
+]);
